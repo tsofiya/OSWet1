44248426e7f145bd4dd6898b365f7ec200cd5a58
tsofiya <tsofiya.waldman@gmail.com>
2019-11-17 20:29:25 +0000

    added files

---
+++ skeleton/cmake-build-debug/CMakeFiles/skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton.dir/smash.cpp.o"
+  "skeleton"
+  "skeleton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
