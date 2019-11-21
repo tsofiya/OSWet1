c72ed39c6da17e03aa87e39c3c1cf4dcb0bb4399
clarafrydman <48757650+clarafrydman@users.noreply.github.com>
2019-11-21 14:31:19 +0200

    Added functions to JobEntry
    updated jobEntry class functions in Commands.cpp, in order to work on jobsList
    Added <time.h> for elapsed time managment

--- skeleton/Commands.cpp
+++ skeleton/Commands.cpp
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 #include <string.h>
 #include <iostream>
 #include <vector>
@@ -6,6 +7,7 @@
 #include <sys/wait.h>
 #include <iomanip>
 #include "Commands.h"
+#include <time.h>
 
 using namespace std;
 
@@ -21,37 +23,45 @@ using namespace std;
 #endif
 
 
+typedef enum {
+    FOREGROUND_JOB,
+    BACKGROUND_JOB,
+    STOPPED_JOB
+}JobStatus;
+
+
+
 const std::string WHITESPACE = " \n\r\t\f\v";
 string _ltrim(const std::string& s)
 {
-  size_t start = s.find_first_not_of(WHITESPACE);
-  return (start == std::string::npos) ? "" : s.substr(start);
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
 }
 
 string _rtrim(const std::string& s)
 {
-  size_t end = s.find_last_not_of(WHITESPACE);
-  return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
 
 string _trim(const std::string& s)
 {
-  return _rtrim(_ltrim(s));
+    return _rtrim(_ltrim(s));
 }
 
 int _parseCommandLine(const char* cmd_line, char** args) {
-  FUNC_ENTRY()
-  int i = 0;
-  std::istringstream iss(_trim(string(cmd_line)).c_str());
-  for(std::string s; iss >> s; ) {
-    args[i] = (char*)malloc(s.length()+1);
-    memset(args[i], 0, s.length()+1);
-    strcpy(args[i], s.c_str());
-    args[++i] = NULL;
-  }
-  return i;
+    FUNC_ENTRY()
+    int i = 0;
+    std::istringstream iss(_trim(string(cmd_line)).c_str());
+    for(std::string s; iss >> s; ) {
+        args[i] = (char*)malloc(s.length()+1);
+        memset(args[i], 0, s.length()+1);
+        strcpy(args[i], s.c_str());
+        args[++i] = NULL;
+    }
+    return i;
 
-  FUNC_EXIT()
+    FUNC_EXIT()
 }
 
 class Command {
@@ -89,36 +99,35 @@ public:
     args[++i] = NULL;
   }
   return i;
-
   FUNC_EXIT()
 }
 */
 bool _isBackgroundComamnd(const char* cmd_line) {
-  const string whitespace = " \t\n";
-  const string str(cmd_line);
-  return str[str.find_last_not_of(whitespace)] == '&';
+    const string whitespace = " \t\n";
+    const string str(cmd_line);
+    return str[str.find_last_not_of(whitespace)] == '&';
 }
 
 void _removeBackgroundSign(char* cmd_line) {
-  const string whitespace = " \t\n";
-  const string str(cmd_line);
-  // find last character other than spaces
-  size_t idx = str.find_last_not_of(whitespace);
-  // if all characters are spaces then return
-  if (idx == string::npos) {
-    return;
-  }
-  // if the command line does not end with & then return
-  if (cmd_line[idx] != '&') {
-    return;
-  }
-  // replace the & (background sign) with space and then remove all tailing spaces.
-  cmd_line[idx] = ' ';
-  // truncate the command line string up to the last non-space character
-  cmd_line[str.find_last_not_of(whitespace, idx-1) + 1] = 0;
+    const string whitespace = " \t\n";
+    const string str(cmd_line);
+    // find last character other than spaces
+    size_t idx = str.find_last_not_of(whitespace);
+    // if all characters are spaces then return
+    if (idx == string::npos) {
+        return;
+    }
+    // if the command line does not end with & then return
+    if (cmd_line[idx] != '&') {
+        return;
+    }
+    // replace the & (background sign) with space and then remove all tailing spaces.
+    cmd_line[idx] = ' ';
+    // truncate the command line string up to the last non-space character
+    cmd_line[str.find_last_not_of(whitespace, idx-1) + 1] = 0;
 }
 
-// TODO: Add your implementation for classes in Commands.h 
+// TODO: Add your implementation for classes in Commands.h
 
 SmallShell::SmallShell() {
 // TODO: add your implementation
@@ -132,7 +141,7 @@ SmallShell::~SmallShell() {
 * Creates and returns a pointer to Command class which matches the given command line (cmd_line)
 */
 Command * SmallShell::CreateCommand(const char* cmd_line) {
-	// For example:
+    // For example:
 /*
   string cmd_s = string(cmd_line);
   if (cmd_s.find("pwd") == 0) {
@@ -144,11 +153,11 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
     return new ExternalCommand(cmd_line);
   }
   */
-  return nullptr;
+    return nullptr;
 }
 
 void SmallShell::executeCommand(const char *cmd_line) {
-  // TODO: Add your implementation here
+    // TODO: Add your implementation here
 
     char** args;
     bool bkg=_isBackgroundComamnd(cmd_line);
@@ -170,7 +179,137 @@ void SmallShell::executeCommand(const char *cmd_line) {
 
 
     // for example:
-  // Command* cmd = CreateCommand(cmd_line);
-  // cmd->execute();
-  // Please note that you must fork smash process for some commands (e.g., external commands....)
+    // Command* cmd = CreateCommand(cmd_line);
+    // cmd->execute();
+    // Please note that you must fork smash process for some commands (e.g., external commands....)
 }
+
+class JobsList {
+public:
+    class JobEntry {
+        // TODO: Add your data members
+    private:
+        int jobPID;
+        int jobSeqID;
+        char* jobcommand;
+        time_t jobAddingTime;
+        JobStatus status;
+
+    public:
+        JobEntry(int PID, int SeqID, char* command):jobPID(PID), jobSeqID(SeqID), JobStatus(BACKGROUND_JOB){
+            jobcommand = new char[(strlen(command))+1];
+            strcpy(jobcommand, command);
+            jobAddingTime=time();
+        }
+        ~JobEntry(){
+            delete[](jobcommand);
+        }
+
+        ostream& operator<<(ostream& os, const JobEntry& jobentry){
+            if (jobentry.status == STOPPED_JOB){
+
+                double secondsElapsed = difftime(time(), jobAddingTime);
+                cout << jobentry.jobSeqID << " " << jobentry.jobcommand << " : " << jobentry.jobPID << " " << secondsElapsed << "(stopped)" << endl;
+            }
+            else{
+                cout << jobentry.jobSeqID << " " << jobentry.jobcommand << " : " << jobentry.jobPID << " " << jobentry.seconds << endl;
+            }
+            return os;
+        }
+
+    };
+
+        private:
+
+
+    // TODO: Add your data members
+public:
+    JobsList();
+    ~JobsList();
+    void addJob(Command* cmd, bool isStopped = false);
+    void printJobsList();
+    void killAllJobs();
+    void removeFinishedJobs();
+    JobEntry * getJobById(int jobId);
+    void removeJobById(int jobId);
+    JobEntry * getLastJob(int* lastJobId);
+    JobEntry *getLastStoppedJob(int *jobId);
+    // TODO: Add extra methods or modify exisitng ones as needed
+};
+
+
+class CommandsHistory {
+protected:
+    class CommandHistoryEntry {
+    private:
+        int seqNum;
+        char* command;
+    public:
+        CommandHistoryEntry(int seq, char*comm):seqNum(seq){
+            command = new char[strlen(comm)+1];
+            strcpy(command, comm);
+        }
+
+        ~CommandHistoryEntry(){
+            delete[](command);
+        }
+
+        void repeatCommand(){
+            seqNum++;
+        }
+
+        int compareCommand(char*comm){
+            return strcmp((comm, command));
+        }
+
+        ostream& operator<<(ostream& os, const CommandHistoryEntry& dt){
+            cout << right << setw(5) << seqNum << " " << command << endl;
+            return os;
+        }
+    };
+
+private:
+    CommandHistoryEntry history[50];
+    int top=0;
+    int capcitcy=0;
+    int seq=1;
+
+    // TODO: Add your data members
+public:
+    CommandsHistory(){}
+    ~CommandsHistory() {}
+
+    void addRecord(const char* cmd_line){
+        if(history[top].compareCommand(cmd_line)==0)
+            history[top].repeatCommand();
+        else
+            history[top]= CommandHistoryEntry(seq, cmd_line);
+        if (top++==50)
+            top=0;
+
+        seq++;
+        if (capcitcy!=49)
+            capcitcy++;
+
+    }
+
+    void printHistory(){
+        for (int i= topd; i<=capcitcy; ++i){
+            cout<< history[i];
+        }
+
+        for (int j = 0; j < top; ++j) {
+            out<< history[j];
+        }
+    }
+};
+
+
+class GetCurrDirCommand : public BuiltInCommand {
+public:
+    GetCurrDirCommand(const char* cmd_line){}
+    virtual ~GetCurrDirCommand() {}
+    void execute() override{
+        cout<< getcwd();
+    }
+};
--- skeleton/Commands.h
+++ skeleton/Commands.h
@@ -1,4 +1,5 @@
 #ifndef SMASH_COMMAND_H_
+
 #define SMASH_COMMAND_H_
 
 #include <vector>
@@ -9,65 +10,65 @@
 #define HISTORY_MAX_RECORDS (50)
 
 class Command {
- public:
-  Command(const char* cmd_line){};
-  virtual ~Command(){  }
-  virtual void execute() = 0;
-  //virtual void prepare();
-  //virtual void cleanup();
-  // TODO: Add your extra methods if needed
+public:
+    Command(const char* cmd_line){};
+    virtual ~Command(){  }
+    virtual void execute() = 0;
+    //virtual void prepare();
+    //virtual void cleanup();
+    // TODO: Add your extra methods if needed
 };
 
 class BuiltInCommand : public Command {
- public:
-  BuiltInCommand(const char* cmd_line);
-  virtual ~BuiltInCommand() {}
+public:
+    BuiltInCommand(const char* cmd_line);
+    virtual ~BuiltInCommand() {}
 };
 
 class ExternalCommand : public Command {
- public:
-  ExternalCommand(const char* cmd_line);
-  virtual ~ExternalCommand() {}
-  void execute() override;
+public:
+    ExternalCommand(const char* cmd_line);
+    virtual ~ExternalCommand() {}
+    void execute() override;
 };
 
 class PipeCommand : public Command {
-  // TODO: Add your data members
- public:
-  PipeCommand(const char* cmd_line);
-  virtual ~PipeCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    PipeCommand(const char* cmd_line);
+    virtual ~PipeCommand() {}
+    void execute() override;
 };
 
 class RedirectionCommand : public Command {
- // TODO: Add your data members
- public:
-  explicit RedirectionCommand(const char* cmd_line);
-  virtual ~RedirectionCommand() {}
-  void execute() override;
-  //void prepare() override;
-  //void cleanup() override;
+    // TODO: Add your data members
+public:
+    explicit RedirectionCommand(const char* cmd_line);
+    virtual ~RedirectionCommand() {}
+    void execute() override;
+    //void prepare() override;
+    //void cleanup() override;
 };
 
 class ChangeDirCommand : public BuiltInCommand {
 // TODO: Add your data members public:
-  ChangeDirCommand(const char* cmd_line, char** plastPwd);
-  virtual ~ChangeDirCommand() {}
-  void execute() override;
+    ChangeDirCommand(const char* cmd_line, char** plastPwd);
+    virtual ~ChangeDirCommand() {}
+    void execute() override;
 };
 
 class GetCurrDirCommand : public BuiltInCommand {
- public:
-  GetCurrDirCommand(const char* cmd_line);
-  virtual ~GetCurrDirCommand() {}
-  void execute() override;
+public:
+    GetCurrDirCommand(const char* cmd_line);
+    virtual ~GetCurrDirCommand() {}
+    void execute() override;
 };
 
 class ShowPidCommand : public BuiltInCommand {
- public:
-  ShowPidCommand(const char* cmd_line);
-  virtual ~ShowPidCommand() {}
-  void execute() override;
+public:
+    ShowPidCommand(const char* cmd_line);
+    virtual ~ShowPidCommand() {}
+    void execute() override;
 };
 
 
@@ -77,129 +78,114 @@ class QuitCommand : public BuiltInCommand {
 // TODO: Add your data members
 protected:
 public:
-  QuitCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~QuitCommand() {}
-  void execute() override;
+    QuitCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~QuitCommand() {}
+    void execute() override;
 };
 
 class CommandsHistory {
- protected:
-  class CommandHistoryEntry {
-	  // TODO: Add your data members
-  };
- // TODO: Add your data members
- public:
-  CommandsHistory();
-  ~CommandsHistory() {}
-  void addRecord(const char* cmd_line);
-  void printHistory();
+protected:
+    class CommandHistoryEntry {
+        // TODO: Add your data members
+    };
+    // TODO: Add your data members
+public:
+    CommandsHistory();
+    ~CommandsHistory() {}
+    void addRecord(const char* cmd_line);
+    void printHistory();
 };
 
 class HistoryCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  HistoryCommand(const char* cmd_line, CommandsHistory* history);
-  virtual ~HistoryCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    HistoryCommand(const char* cmd_line, CommandsHistory* history);
+    virtual ~HistoryCommand() {}
+    void execute() override;
 };
 
+
+        // TODO: Add your data members
+
 class JobsList {
- public:
-  class JobEntry {
+        public:
+        class JobEntry {
 
-      int jobPID;
-      int jobSeqID;
-      char* jobcommand;
-      int seconds;
+            // TODO: Add your data members
+        };
 
-      JobEntry(int jobPID, int jobSeqID, const char* jobcomm){
-        this->jobPID=jobPID;
-        this->jobSeqID=jobSeqID;
-        /*
-        this->jobcommand= (char*)malloc(strlen(jobcomm)+1);
-        if(!jobcommand){
-          ; //???????????
-        }
-         */
-        this->seconds=0; //not sure this should be initiated here...
-      }
-
-      ~
-
-   // TODO: Add your data members
-  };
- // TODO: Add your data members
- public:
-  JobsList();
-  ~JobsList();
-  void addJob(Command* cmd, bool isStopped = false);
-  void printJobsList();
-  void killAllJobs();
-  void removeFinishedJobs();
-  JobEntry * getJobById(int jobId);
-  void removeJobById(int jobId);
-  JobEntry * getLastJob(int* lastJobId);
-  JobEntry *getLastStoppedJob(int *jobId);
-  // TODO: Add extra methods or modify exisitng ones as needed
+    // TODO: Add your data members
+public:
+    JobsList();
+    ~JobsList();
+    void addJob(Command* cmd, bool isStopped = false);
+    void printJobsList();
+    void killAllJobs();
+    void removeFinishedJobs();
+    JobEntry * getJobById(int jobId);
+    void removeJobById(int jobId);
+    JobEntry * getLastJob(int* lastJobId);
+    JobEntry *getLastStoppedJob(int *jobId);
+    // TODO: Add extra methods or modify exisitng ones as needed
 };
 
 class JobsCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  JobsCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~JobsCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    JobsCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~JobsCommand() {}
+    void execute() override;
 };
 
 class KillCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  KillCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~KillCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    KillCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~KillCommand() {}
+    void execute() override;
 };
 
 class ForegroundCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  ForegroundCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~ForegroundCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    ForegroundCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~ForegroundCommand() {}
+    void execute() override;
 };
 
 class BackgroundCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  BackgroundCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~BackgroundCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    BackgroundCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~BackgroundCommand() {}
+    void execute() override;
 };
 
 class CopyCommand : public BuiltInCommand {
- public:
-  CopyCommand(const char* cmd_line);
-  virtual ~CopyCommand() {}
-  void execute() override;
+public:
+    CopyCommand(const char* cmd_line);
+    virtual ~CopyCommand() {}
+    void execute() override;
 };
 
 
 class SmallShell {
- private:
-  // TODO: Add your data members
-  SmallShell();
- public:
-  Command *CreateCommand(const char* cmd_line);
-  SmallShell(SmallShell const&)      = delete; // disable copy ctor
-  void operator=(SmallShell const&)  = delete; // disable = operator
-  static SmallShell& getInstance() // make SmallShell singleton
-  {
-    static SmallShell instance; // Guaranteed to be destroyed.
-    // Instantiated on first use.
-    return instance;
-  }
-  ~SmallShell();
-  void executeCommand(const char* cmd_line);
-  // TODO: add extra methods as needed
+private:
+    // TODO: Add your data members
+    SmallShell();
+public:
+    Command *CreateCommand(const char* cmd_line);
+    SmallShell(SmallShell const&)      = delete; // disable copy ctor
+    void operator=(SmallShell const&)  = delete; // disable = operator
+    static SmallShell& getInstance() // make SmallShell singleton
+    {
+        static SmallShell instance; // Guaranteed to be destroyed.
+        // Instantiated on first use.
+        return instance;
+    }
+    ~SmallShell();
+    void executeCommand(const char* cmd_line);
+    // TODO: add extra methods as needed
 };
 
 #endif //SMASH_COMMAND_H_
